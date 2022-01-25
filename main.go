f39fdf4054cc8c6d40bd5300b1860a843498a588
drhax9908 <drhax9908@users.noreply.github.com>
2022-01-24 18:13:41 -0800

    sentry removal part 2

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"Meme_Api/libraries/redis"
 	"Meme_Api/server"
 
-	"github.com/getsentry/sentry-go"
 )
 
 func main() {
@@ -17,15 +16,6 @@ func main() {
 	// Initialize Libraries
 	reddit.Init()
 	redis.Init()
-
-	// Initialize and defer Sentry
-	if err := sentry.Init(sentry.ClientOptions{
-		Dsn: os.Getenv("SENTRY_DSN"),
-	}); err != nil {
-		log.Fatalln("Sentry Init Error: ", err)
-	}
-
-	defer sentry.Flush(2 * time.Second)
 
 	// Initialize Server
 	server.Init()
