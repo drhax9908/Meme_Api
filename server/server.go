883126b0a252349b65232f16e04c1a8a281c390e
drhax9908 <drhax9908@users.noreply.github.com>
2022-01-24 18:31:48 -0800

    death to sentry

--- server/server.go
+++ server/server.go
@@ -1,13 +1,9 @@
 package server
 
-import "github.com/getsentry/sentry-go"
 
 // Init : Initialize the routes and server
 func Init() {
 	r := NewRouter()
-	err := r.Run()
+	r.Run()
 
-	if err != nil {
-		sentry.CaptureException(err)
-	}
 }
